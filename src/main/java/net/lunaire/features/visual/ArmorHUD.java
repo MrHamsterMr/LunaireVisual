9c1b70108b55f4470957ac078f9b91219f8a58c6
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 16:32:42 +0400

    Update ArmorHUD.java

--- src/main/java/net/lunaire/features/visual/ArmorHUD.java
+++ src/main/java/net/lunaire/features/visual/ArmorHUD.java
@@ -1,12 +1,16 @@
 package net.lunaire.features.visual;
+
 import net.lunaire.core.Category;
-import net.lunaire.core.Module;
+import net.lunaire.core.LunaireModule;
 import net.minecraft.client.gui.DrawContext;
 
-public class ArmorHUD extends Module {
-    public ArmorHUD() { super("ArmorHUD", Category.HUD, 0); }
+public class ArmorHUD extends LunaireModule {
+    public ArmorHUD() { 
+        super("ArmorHUD", Category.HUD, 0); 
+    }
+
     @Override
     public void onRenderHud(DrawContext context) {
-        // Твой код отрисовки брони
+        // Логика отрисовки уже есть в ModuleManager, но для чистоты оставляем метод здесь
     }
 }
