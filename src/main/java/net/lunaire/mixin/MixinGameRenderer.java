fe2b05fe450a897c8f28a814b5e7d332e15e3565
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 15:39:50 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -11,14 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
-
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
     private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Float> info) {
         Module m = ModuleManager.getModule("Zoom");
         if (m != null && m.isEnabled()) {
-            // В твоем Legacy лаунчере FOV — это Float (согласно твоему логу)
-            float originalFov = info.getReturnValue();
-            info.setReturnValue(originalFov / 4.0f);
+            float fov = info.getReturnValue();
+            info.setReturnValue(fov / 4.0f);
         }
     }
 }
