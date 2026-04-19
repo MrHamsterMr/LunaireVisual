2655e1f701c3f61a29c258cc34dad3c132c3762c
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:41:18 +0400

    Update Zoom.java

--- src/main/java/net/lunaire/features/visual/Zoom.java
+++ src/main/java/net/lunaire/features/visual/Zoom.java
@@ -1,9 +1,7 @@
 package net.lunaire.features.visual;
-
 import net.lunaire.core.Category;
 import net.lunaire.core.Module;
 
 public class Zoom extends Module {
-    public Zoom() { super("Zoom", Category.VISUAL); }
-    // Логика зума работает через MixinGameRenderer, проверяя этот модуль
+    public Zoom() { super("Zoom", Category.VISUAL, 0); }
 }
