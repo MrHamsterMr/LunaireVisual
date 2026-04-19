4df6b73289e15c6fa5239f315a2a1652491bc1f1
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 11:41:00 +0400

    Update IMinecraftClient.java

--- src/main/java/net/lunaire/mixin/IMinecraftClient.java
+++ src/main/java/net/lunaire/mixin/IMinecraftClient.java
@@ -1,10 +1,15 @@
 package net.lunaire.mixin;
+
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MinecraftClient.class)
 public interface IMinecraftClient {
+    // Этот метод позволяет нам изменять скрытую переменную itemUseCooldown
     @Accessor("itemUseCooldown")
     void setItemUseCooldown(int cooldown);
+
+    @Accessor("itemUseCooldown")
+    int getItemUseCooldown();
 }
