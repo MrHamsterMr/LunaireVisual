55b17c2f8446f816185872c0fdffd760e4d19764
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 18:39:14 +0400

    Update LunaireClient.java

--- src/main/java/net/lunaire/core/LunaireClient.java
+++ src/main/java/net/lunaire/core/LunaireClient.java
@@ -1,11 +1,9 @@
 package net.lunaire.core;
-
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.lunaire.features.ModuleManager;
 import net.lunaire.ui.ClickGuiScreen;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import java.util.ArrayList;
@@ -18,32 +16,19 @@ public class LunaireClient implements ClientModInitializer {
     public void onInitializeClient() {
         ModuleManager.init();
         Config.load();
-
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
             long win = client.getWindow().getHandle();
-
             if (InputUtil.isKeyPressed(win, GLFW.GLFW_KEY_RIGHT_SHIFT)) {
-                if (!(client.currentScreen instanceof ClickGuiScreen)) {
-                    client.setScreen(new ClickGuiScreen());
-                }
+                if (!(client.currentScreen instanceof ClickGuiScreen)) client.setScreen(new ClickGuiScreen());
             }
-
             for (LunaireModule m : ModuleManager.getModules()) {
                 if (m.key != 0) {
                     boolean down = m.isMouse ? GLFW.glfwGetMouseButton(win, m.key) == GLFW.GLFW_PRESS : InputUtil.isKeyPressed(win, m.key);
-                    if (down && !PRESSED.contains(m.key)) {
-                        m.toggle();
-                        PRESSED.add(m.key);
-                    } else if (!down) PRESSED.remove(Integer.valueOf(m.key));
+                    if (down && !PRESSED.contains(m.key)) { m.toggle(); PRESSED.add(m.key); }
+                    else if (!down) PRESSED.remove(Integer.valueOf(m.key));
                 }
-                if (m.isEnabled()) m.onTick();
-            }
-        });
-
-        HudRenderCallback.EVENT.register((context, tickDelta) -> {
-            for (LunaireModule m : ModuleManager.getModules()) {
-                if (m.isEnabled()) m.onRenderHud(context);
+                if (m.enabled) m.onTick();
             }
         });
     }
