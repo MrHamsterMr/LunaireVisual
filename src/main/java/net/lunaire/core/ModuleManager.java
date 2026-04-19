767d4bc48f5abd5978c7b278ba292e4562873fd6
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:07:04 +0400

    Update ModuleManager.java

--- src/main/java/net/lunaire/core/ModuleManager.java
+++ src/main/java/net/lunaire/core/ModuleManager.java
@@ -1,19 +1,24 @@
 package net.lunaire.core;
 
-import net.lunaire.features.combat.*;
-import net.lunaire.features.visual.*;
-import java.util.*;
+import net.lunaire.features.visual.ArmorHUD; // ВАЖНО: Добавлен импорт
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModuleManager {
     private static final List<Module> modules = new ArrayList<>();
 
     public static void init() {
-        // Добавляем модули из разных папок
-        modules.add(new FastExp());
-        modules.add(new Zoom());
+        modules.clear();
+        
+        // Теперь компилятор найдет этот класс, так как мы его импортировали выше
         modules.add(new ArmorHUD());
-        // Сюда будешь дописывать новые классы
+        
+        // Если хочешь добавить пустые модули для теста без создания файлов:
+        modules.add(new Module("Zoom", Category.VISUAL) {});
+        modules.add(new Module("FastExp", Category.COMBAT) {});
     }
 
-    public static List<Module> getModules() { return modules; }
+    public static List<Module> getModules() {
+        return modules;
+    }
 }
