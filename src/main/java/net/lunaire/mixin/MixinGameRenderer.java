4834870b45560dc3f161c20eeae1476ee2102558
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:18:37 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -1,7 +1,8 @@
 package net.lunaire.mixin;
 
-import net.lunaire.features.ModuleManager;
 import net.lunaire.core.Module;
+import net.lunaire.features.ModuleManager;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,12 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinGameRenderer {
 
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void onGetFov(float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> info) {
-        // Ищем модуль Zoom через наш менеджер
+    private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> info) {
+        // Проверяем включен ли модуль Zoom в ModuleManager
         for (Module m : ModuleManager.getModules()) {
-            if (m.getName().equals("Zoom") && m.isEnabled()) {
-                // Если зум включен, делим текущий FOV на 4 (приближаем)
-                info.setReturnValue(info.getReturnValue() / 4.0);
+            if (m.getName().equalsIgnoreCase("Zoom") && m.isEnabled()) {
+                // В 1.21.4 возвращаемое значение - Double
+                double originalFov = info.getReturnValue();
+                info.setReturnValue(originalFov / 4.0);
+                break;
             }
         }
     }
