b35f3342c5a6dccc52bd2b9bc40618a2b679c83e
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 13:59:32 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -15,6 +15,7 @@ public class MixinGameRenderer {
     private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Float> info) {
         Module m = ModuleManager.getModule("Zoom");
         if (m != null && m.isEnabled()) {
+            // В твоей сборке FOV — это Float.
             float originalFov = info.getReturnValue();
             info.setReturnValue(originalFov / 4.0f);
         }
