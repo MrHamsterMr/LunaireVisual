4db044c9ac4de9593bacaca955f9165a30d3a5ce
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 13:24:52 +0400

    Update LunaireClient.java

--- src/main/java/net/lunaire/core/LunaireClient.java
+++ src/main/java/net/lunaire/core/LunaireClient.java
@@ -2,47 +2,22 @@ package net.lunaire.core;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.lunaire.features.ModuleManager;
 import net.lunaire.ui.ClickGuiScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class LunaireClient implements ClientModInitializer {
-    private static final MinecraftClient mc = MinecraftClient.getInstance();
     private static final List<Integer> PRESSED_KEYS = new ArrayList<>();
 
     @Override
     public void onInitializeClient() {
         ModuleManager.init();
-
-        // Система команд (.bind)
-        ClientSendMessageEvents.ALLOW_CHAT.register(message -> {
-            if (message.startsWith(".bind")) {
-                String[] args = message.split(" ");
-                if (args.length < 3) {
-                    mc.player.sendMessage(Text.of("§b[Lunaire] §7Используй: §f.bind <модуль> <кнопка>"), false);
-                } else {
-                    Module m = ModuleManager.getModule(args[1]);
-                    if (m != null) {
-                        // Упрощенный бинд (берет первую букву)
-                        int key = GLFW.glfwGetKeyName(GLFW.GLFW_KEY_A, 0) != null ? args[2].toUpperCase().charAt(0) : 0;
-                        m.setKey(key); 
-                        mc.player.sendMessage(Text.of("§b[Lunaire] §7Бинд установлен!"), false);
-                    } else {
-                        mc.player.sendMessage(Text.of("§b[Lunaire] §cМодуль не найден!"), false);
-                    }
-                }
-                return false; 
-            }
-            return true;
-        });
+        Config.load(); // ЗАГРУЖАЕМ НАСТРОЙКИ
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
@@ -62,6 +37,7 @@ public class LunaireClient implements ClientModInitializer {
                         PRESSED_KEYS.remove(Integer.valueOf(m.getKey()));
                     }
                 }
+                // ВАЖНО: Вызов логики функций
                 if (m.isEnabled()) m.onTick();
             }
         });
