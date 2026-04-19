20901cdcf79c923d5ce37ed0dd499b1c00c6585b
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 18:42:48 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -3,32 +3,32 @@ package net.lunaire.mixin;
 import net.lunaire.core.LunaireModule;
 import net.lunaire.core.Setting;
 import net.lunaire.features.ModuleManager;
-import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
-    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Float> info) {
-        LunaireModule m = ModuleManager.getModule("Zoom");
-        if (m != null && m.isEnabled()) {
-            info.setReturnValue(info.getReturnValue() / 4.0f);
-        }
-    }
-
+    /**
+     * Отключение тряски камеры при получении урона (NoHurtTilt)
+     * Это часть функции NoRender.
+     */
     @Inject(method = "bobViewWhenHurt", at = @At("HEAD"), cancellable = true)
     private void onHurtTilt(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        // Ищем наш новый класс LunaireModule
         LunaireModule m = ModuleManager.getModule("NoRender");
+        
         if (m != null && m.isEnabled()) {
+            // Ищем настройку NoShake внутри модуля NoRender
             Setting s = m.getSetting("NoShake");
-            if (s != null && s.bVal) ci.cancel();
+            if (s != null && s.bVal) {
+                // Если галочка включена, отменяем (cancel) тряску камеры
+                ci.cancel();
+            }
         }
     }
 }
