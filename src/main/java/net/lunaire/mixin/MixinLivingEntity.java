61f4cfc6f48823e9f5087d8af7549220c7e3d28b
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 18:41:16 +0400

    Update MixinLivingEntity.java

--- src/main/java/net/lunaire/mixin/MixinLivingEntity.java
+++ src/main/java/net/lunaire/mixin/MixinLivingEntity.java
@@ -1,7 +1,5 @@
 package net.lunaire.mixin;
-
 import net.lunaire.core.LunaireModule;
-import net.lunaire.core.Setting;
 import net.lunaire.features.ModuleManager;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,11 +9,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
-    @Inject(method = "getJumpVelocity", at = @At("HEAD")) // Заглушка для доступа к логике рендера
+    @Inject(method = "getJumpVelocity", at = @At("HEAD"))
     private void onHit(CallbackInfoReturnable<Float> cir) {
         LunaireModule m = ModuleManager.getModule("HitColor");
-        if (m != null && m.isEnabled()) {
-            // В 1.21.4 цвет меняется через оверлей в Renderer
-        }
+        if (m != null && m.enabled) { /* База */ }
     }
 }
