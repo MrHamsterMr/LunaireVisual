e810569b5c369808e8c8cec625d6101e97f9b287
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 16:32:30 +0400

    Update FullBright.java

--- src/main/java/net/lunaire/features/visual/FullBright.java
+++ src/main/java/net/lunaire/features/visual/FullBright.java
@@ -1,20 +1,26 @@
 package net.lunaire.features.visual;
 
 import net.lunaire.core.Category;
-import net.lunaire.core.Module;
+import net.lunaire.core.LunaireModule;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.effect.StatusEffectInstance;
 
-public class FullBright extends Module {
+public class FullBright extends LunaireModule {
     public FullBright() {
         super("FullBright", Category.VISUAL, 0);
     }
 
     @Override
     public void onTick() {
-        mc.options.getGamma().setValue(100.0);
+        if (mc.player != null) {
+            mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1000, 0, false, false));
+        }
     }
 
     @Override
     public void onDisable() {
-        mc.options.getGamma().setValue(1.0);
+        if (mc.player != null) {
+            mc.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+        }
     }
 }
