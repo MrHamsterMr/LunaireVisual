682344b1e22f413a3f72b3de0bfcdb27d3ddff63
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 11:29:50 +0400

    Create Module.java

---
+++ src/main/java/net/lunaire/core/Module.java
@@ -0,0 +1,26 @@
+package net.lunaire.core;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+
+public abstract class Module {
+    protected static final MinecraftClient mc = MinecraftClient.getInstance();
+    private String name;
+    private Category category;
+    private boolean enabled = false;
+
+    public Module(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() { return name; }
+    public Category getCategory() { return category; }
+    public boolean isEnabled() { return enabled; }
+    public void toggle() { this.enabled = !this.enabled; }
+
+    public void onTick() {}
+    public void onRenderHud(DrawContext context) {}
+
+    public enum Category { COMBAT, VISUAL, MISC, HUD }
+}
