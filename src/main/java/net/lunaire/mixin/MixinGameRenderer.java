88c29f89b7ee10e0389a834a0b511b5ce4d3b519
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 13:17:53 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -13,9 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinGameRenderer {
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
     private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> info) {
-        Module zoom = ModuleManager.getModule("Zoom");
-        if (zoom != null && zoom.isEnabled()) {
-            // Прямое использование double без явного каста float, так как 1.21.4 использует double
+        Module m = ModuleManager.getModule("Zoom");
+        if (m != null && m.isEnabled()) {
             double originalFov = info.getReturnValue();
             info.setReturnValue(originalFov / 4.0);
         }
