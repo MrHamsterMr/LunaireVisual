872156914523ef0371a09bdfbe9aca5333cae12f
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:01:20 +0400

    Update LunaireClient.java

--- src/main/java/net/lunaire/core/LunaireClient.java
+++ src/main/java/net/lunaire/core/LunaireClient.java
@@ -3,7 +3,7 @@ package net.lunaire.core;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.lunaire.features.ModuleManager;
+import net.lunaire.features.ModuleManager; // Вот этот импорт вызывал ошибку
 import net.lunaire.ui.ClickGuiScreen;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -15,8 +15,8 @@ public class LunaireClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
-            ModuleManager.getModules().forEach(m -> { if (m.isEnabled()) m.onTick(); });
-
+            
+            // Открытие меню на Правый Шифт
             if (InputUtil.isKeyPressed(client.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT)) {
                 if (!(client.currentScreen instanceof ClickGuiScreen)) {
                     client.setScreen(new ClickGuiScreen());
@@ -25,7 +25,7 @@ public class LunaireClient implements ClientModInitializer {
         });
 
         HudRenderCallback.EVENT.register((context, tickDelta) -> {
-            ModuleManager.getModules().forEach(m -> { if (m.isEnabled()) m.onRenderHud(context); });
+            context.drawText(net.minecraft.client.MinecraftClient.getInstance().textRenderer, "Lunaire Loaded", 10, 10, -1, true);
         });
     }
 }
