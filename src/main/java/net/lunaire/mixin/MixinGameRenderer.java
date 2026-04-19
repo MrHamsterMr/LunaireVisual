24ac2d909b921748f76fe8cbea9e8cee39e7a292
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 16:48:12 +0400

    Update MixinGameRenderer.java

--- src/main/java/net/lunaire/mixin/MixinGameRenderer.java
+++ src/main/java/net/lunaire/mixin/MixinGameRenderer.java
@@ -1,34 +1,21 @@
 package net.lunaire.mixin;
 
 import net.lunaire.core.LunaireModule;
-import net.lunaire.core.Setting;
 import net.lunaire.features.ModuleManager;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
-
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
     private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Float> info) {
         LunaireModule m = ModuleManager.getModule("Zoom");
         if (m != null && m.isEnabled()) {
             info.setReturnValue(info.getReturnValue() / 4.0f);
-        }
-    }
-
-    @Inject(method = "bobViewWhenHurt", at = @At("HEAD"), cancellable = true)
-    private void onHurtTilt(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-        LunaireModule m = ModuleManager.getModule("NoRender");
-        if (m != null && m.isEnabled()) {
-            Setting s = m.getSetting("NoShake");
-            if (s != null && s.bVal) ci.cancel();
         }
     }
 }
