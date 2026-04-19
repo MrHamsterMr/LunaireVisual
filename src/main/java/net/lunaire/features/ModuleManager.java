3a896afcf817f3df9220c2f762c3c7f990e32b19
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 14:34:48 +0400

    Update ModuleManager.java

--- src/main/java/net/lunaire/features/ModuleManager.java
+++ src/main/java/net/lunaire/features/ModuleManager.java
@@ -4,7 +4,6 @@ import net.lunaire.core.Category;
 import net.lunaire.core.Module;
 import net.lunaire.mixin.IMinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import org.lwjgl.glfw.GLFW;
@@ -16,40 +15,15 @@ public class ModuleManager {
 
     public static void init() {
         modules.clear();
-        
-        // --- COMBAT ---
-        modules.add(new Module("FastExp", Category.COMBAT, 0) {
-            @Override public void onTick() {
-                if (mc.options.useKey.isPressed() && mc.player.getMainHandStack().isOf(Items.EXPERIENCE_BOTTLE)) {
-                    ((IMinecraftClient)mc).setItemUseCooldown(0);
-                }
-            }
-        });
-        modules.add(new Module("FastSwap", Category.COMBAT, 0) {});
-        modules.add(new Module("TotemPop", Category.COMBAT, 0) {});
-        modules.add(new Module("HitColor", Category.COMBAT, 0) {});
-
-        // --- VISUAL ---
         modules.add(new Module("Zoom", Category.VISUAL, GLFW.GLFW_KEY_C) {});
+        modules.add(new Module("NoRender", Category.VISUAL, 0) {});
         modules.add(new Module("FullBright", Category.VISUAL, GLFW.GLFW_KEY_B) {
             @Override public void onTick() { mc.options.getGamma().setValue(100.0); }
             @Override public void onDisable() { mc.options.getGamma().setValue(1.0); }
         });
-        modules.add(new Module("NoRender", Category.VISUAL, 0) {});
-        modules.add(new Module("TargetHUD", Category.VISUAL, 0) {
-            @Override public void onRenderHud(DrawContext context) {
-                if (mc.targetedEntity instanceof LivingEntity target) {
-                    context.fill(10, 10, 130, 45, 0x90000000);
-                    context.drawText(mc.textRenderer, target.getName().getString(), 15, 15, -1, true);
-                    context.drawText(mc.textRenderer, (int)target.getHealth() + " HP", 15, 25, 0xFF00FBFF, true);
-                }
-            }
-        });
-        
-        // --- HUD ---
         modules.add(new Module("ArmorHUD", Category.HUD, 0) {
             @Override public void onRenderHud(DrawContext context) {
-                int y = 50;
+                int y = 60;
                 for (int i = 3; i >= 0; i--) {
                     ItemStack stack = mc.player.getInventory().getArmorStack(i);
                     if (!stack.isEmpty()) {
@@ -60,16 +34,11 @@ public class ModuleManager {
             }
         });
 
-        // --- MISC ---
-        String[] miscModules = {"Waypoints", "Friends", "ItemScroller", "Optimization", "FreeLook", "BlockOverlay", "Crosshair", "CustomHand", "Macros", "ShulkerView", "HitboxColor"};
-        for(String name : miscModules) {
-            modules.add(new Module(name, Category.MISC, 0) {});
-        }
+        String[] misc = {"Waypoints", "Friends", "ItemScroller", "Optimization", "FreeLook", "FastSwap", "TotemPop"};
+        for (String s : misc) modules.add(new Module(s, Category.MISC, 0) {});
     }
 
-    public static List<Module> getModules() { 
-        return modules; 
-    }
+    public static List<Module> getModules() { return modules; }
 
     public static Module getModule(String name) {
         for (Module m : modules) {
