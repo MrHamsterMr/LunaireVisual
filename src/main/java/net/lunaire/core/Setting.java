40fc1705bdac5ff09a6bb86e4dd54fac9e2998b6
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 16:23:47 +0400

    Update Setting.java

--- src/main/java/net/lunaire/core/Setting.java
+++ src/main/java/net/lunaire/core/Setting.java
@@ -2,10 +2,10 @@ package net.lunaire.core;
 
 public class Setting {
     public String name;
-    public boolean bVal; // Для галочек
-    public double dVal;  // Для чисел
-    public int color;    // Для цветов
-    public String type;  // "bool", "num", "color"
+    public boolean bVal;
+    public double dVal;
+    public int color;
+    public String type;
 
     public Setting(String name, boolean val) { this.name = name; this.bVal = val; this.type = "bool"; }
     public Setting(String name, double val) { this.name = name; this.dVal = val; this.type = "num"; }
