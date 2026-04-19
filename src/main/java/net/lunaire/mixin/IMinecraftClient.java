ab4e78be45cf9bab36690794d0a40bd2a60cfb0d
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 11:32:51 +0400

    Create IMinecraftClient.java

---
+++ src/main/java/net/lunaire/mixin/IMinecraftClient.java
@@ -0,0 +1,10 @@
+package net.lunaire.mixin;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MinecraftClient.class)
+public interface IMinecraftClient {
+    @Accessor("itemUseCooldown")
+    void setItemUseCooldown(int cooldown);
+}
