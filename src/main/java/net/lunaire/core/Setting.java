8992c9d6bd4738c591724e49ca4cc4cbde019af7
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 16:47:29 +0400

    Update Setting.java

--- src/main/java/net/lunaire/core/Setting.java
+++ src/main/java/net/lunaire/core/Setting.java
@@ -1,13 +1,9 @@
 package net.lunaire.core;
-
 public class Setting {
     public String name;
     public boolean bVal;
     public double dVal;
-    public int color;
     public String type;
-
     public Setting(String name, boolean val) { this.name = name; this.bVal = val; this.type = "bool"; }
     public Setting(String name, double val) { this.name = name; this.dVal = val; this.type = "num"; }
-    public Setting(String name, int color) { this.name = name; this.color = color; this.type = "color"; }
 }
