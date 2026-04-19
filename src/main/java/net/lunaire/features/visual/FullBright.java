4056b4279fe7dc9ffdd1492788891bd7442e4e2c
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:31:17 +0400

    Create FullBright.java

---
+++ src/main/java/net/lunaire/features/visual/FullBright.java
@@ -0,0 +1,19 @@
+package net.lunaire.features.visual;
+
+import net.lunaire.core.Category;
+import net.lunaire.core.Module;
+
+public class FullBright extends Module {
+    public FullBright() { super("FullBright", Category.VISUAL, 0); }
+
+    @Override
+    public void onTick() {
+        // Простой способ: ставим гамму на максимум
+        mc.options.getGamma().setValue(100.0);
+    }
+
+    @Override
+    public void onDisable() {
+        mc.options.getGamma().setValue(1.0);
+    }
+}
