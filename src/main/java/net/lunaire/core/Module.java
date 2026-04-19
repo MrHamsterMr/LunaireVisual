49cfcd7b23f0992a16d522e75781afbf663d1a54
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 14:51:19 +0400

    Update Module.java

--- src/main/java/net/lunaire/core/Module.java
+++ src/main/java/net/lunaire/core/Module.java
@@ -1,4 +1,5 @@
 package net.lunaire.core;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
@@ -28,7 +29,6 @@ public abstract class Module {
     public void onRenderHud(DrawContext context) {}
 
     public String getName() { return name; }
-    public Category getCategory() { return category; }
     public boolean isEnabled() { return enabled; }
     public int getKey() { return key; }
     public void setKey(int key) { this.key = key; }
