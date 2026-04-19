ff5ee8b8a8b53519d9df3466d06163bcd0dccb22
MrHamsterMr <lomzikbg@gmail.com>
2026-04-19 12:41:44 +0400

    Update FastExp.java

--- src/main/java/net/lunaire/features/combat/FastExp.java
+++ src/main/java/net/lunaire/features/combat/FastExp.java
@@ -1,20 +1,7 @@
 package net.lunaire.features.combat;
-
 import net.lunaire.core.Category;
 import net.lunaire.core.Module;
-import net.lunaire.mixin.IMinecraftClient; // Импортируем наш миксин
-import net.minecraft.item.Items;
 
 public class FastExp extends Module {
-    public FastExp() { super("FastExp", Category.COMBAT); }
-
-    @Override
-    public void onTick() {
-        if (mc.options.useKey.isPressed()) {
-            if (mc.player.getMainHandStack().isOf(Items.EXPERIENCE_BOTTLE)) {
-                // Магия: превращаем обычный клиент в наш IMinecraftClient и обнуляем КД
-                ((IMinecraftClient)mc).setItemUseCooldown(0);
-            }
-        }
-    }
+    public FastExp() { super("FastExp", Category.COMBAT, 0); }
 }
